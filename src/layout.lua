4445d1e0ef937670bd8ba0b268b13c1fc79c0ae0
xbc5 <87829033+xbc5@users.noreply.github.com>
2023-11-16 23:49:26 +0000

    style: rename variable

--- src/layout.lua
+++ src/layout.lua
@@ -14,24 +14,24 @@ awful.layout.layouts = {
 }
 
 -- Return an imagebox widget which will contain an icon indicating which layout we're using.
--- If necessary, it will also add it to the screen table as "layout_icon".
+-- If necessary, it will also add it to the screen table as "mylayoutbox".
 -- @param s An Awesome screen
 function M.icon(s)
-  if s.layout_icon ~= nil then
-    return s.layout_icon
+  if s.mylayoutbox ~= nil then
+    return s.mylayoutbox
   end
 
-  local layout_icon = awful.widget.layoutbox(s)
+  local mylayoutbox = awful.widget.layoutbox(s)
 
   local a = awful.button({ }, 1, function () awful.layout.inc( 1) end)
   local b = awful.button({ }, 3, function () awful.layout.inc(-1) end)
   local c = awful.button({ }, 4, function () awful.layout.inc( 1) end)
   local d = awful.button({ }, 5, function () awful.layout.inc(-1) end)
 
-  layout_icon:buttons(gears.table.join(a, b , c, d))
+  mylayoutbox:buttons(gears.table.join(a, b , c, d))
 
-  s.layout_icon = layout_icon
-  return s.layout_icon
+  s.mylayoutbox = mylayoutbox
+  return s.mylayoutbox
 end
 
 return M

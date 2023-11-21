539463c0840f8142d09ff4bd947e7dc632081c1d
xbc5 <87829033+xbc5@users.noreply.github.com>
2023-11-21 04:33:07 +0000

    refactor: prepare tag for multi-screen (#2)

--- src/x/tag.lua
+++ src/x/tag.lua
@@ -7,35 +7,43 @@ local x = {
 local M = {
   defaults = {
     -- keep this flat (no nested tables) otherwise do deep copy (see init fn)
+    screen             = awful.screen.primary, -- override this below for multi-screen setups
     layout             = awful.layout.suit.max,
     master_fill_policy = "master_width_factor",
     gap_single_client  = false,
     gap                = 3,
   },
-  specs = {
-    { name = "daily",
-      key = "m",
-      spec = { selected = true, },
+  name = { daily = "daily",
+           dev_b = "dev:b",
+           dev_t = "dev:t",
+           dev_s = "dev-s",
+           dom0  = "dom0",
+         }
+}
+
+M.specs = {
+  { name = M.name.daily,
+    key = "m",
+    spec = { selected = true, },
+  },
+  { name = M.name.dev_b,
+    key = "n",
+    spec = { },
+  },
+  { name = M.name.dev_t,
+    key = "u",
+    spec = {
+      layout = awful.layout.suit.tile,
     },
-    { name = "dev:b",
-      key = "n",
-      spec = { },
-    },
-    { name = "dev:t",
-      key = "u",
-      spec = {
-        layout = awful.layout.suit.tile,
-      },
-    },
-    { name = "dev-s",
-      key = "i",
-      spec = { },
-    },
-    { name = "dom0",
-      key = "s",
-      spec = { },
-    },
-  }
+  },
+  { name = M.name.dev_s,
+    key = "i",
+    spec = { },
+  },
+  { name = M.name.dom0,
+    key = "s",
+    spec = { },
+  },
 }
 
 
@@ -50,8 +58,9 @@ end
 -- Return a tag object given its name
 -- @param name The name of the tag
 -- @param s An optional Aweome screen (defaults to the focused screen)
--- @return a Tag objecy
+-- @return a Tag object
 function M.get(name, s)
+  -- TODO: we shall use a unique set of tags across all screens, so is s==nil, search all screens #2
   return awful.tag.find_by_name(s or awful.screen.focused(), name)
 end
 
@@ -60,6 +69,7 @@ end
 -- @param name The name of the tag
 -- @param s An optional Awesome screen -- see tag.get() for defaults.
 function M.take(c, name, s)
+  -- TODO: remove screen requirements #2
   c:move_to_tag(M.get(name, s))
 end
 
@@ -68,6 +78,7 @@ end
 -- @param s The [optional] screen that the tag resides -- defaults to the focused screen.
 -- @return nil
 function M.view(name, s)
+  -- TODO: remove s #2
   M.get(name, s):view_only()
 end
 
@@ -76,16 +87,21 @@ end
 -- @param s The [optional] screen that the tag resides -- defaults to the focused screen.
 -- @return nil
 function M.move(name, s)
+  -- TODO: remove s #2
   client.focus:move_to_tag(M.get(name, s))
 end
 
-function M.init(s)
+-- Initialise all tags.
+function M.init()
+  if M._initialised then return end
+
   -- merge specs into defaults, then use that
   for _, tg in pairs(M.specs) do
     local spec = gears.table.crush(x.util.shallow_copy(M.defaults), tg.spec)
-    spec.screen = s
     awful.tag.add(tg.name, spec)
   end
+
+  M._initialised = true
 end
 
 return M

e37ae27d7f577ca65f3a84a42e2587ef724d13e2
xbc5 <87829033+xbc5@users.noreply.github.com>
2023-11-24 05:45:28 +0000

    refactor: resize titlebars

--- src/x/titlebar.lua
+++ src/x/titlebar.lua
@@ -7,9 +7,11 @@ local x = {
 
 local M = {}
 
+local SIZE = 10
+
 function M.default_props(c, overrides)
   local defaults = {
-    height = 5,
+    size = SIZE,
     bg_normal = qubes.get_colour(c),
     bg_focus = qubes.get_colour_focus(c),
   }
@@ -19,7 +21,11 @@ end
 client.connect_signal("request::titlebars", function(c)
   -- dev console has titlebars at the bottom
   if string.match(c.class, x.xprop.dev_console.class_p) then
-    awful.titlebar(c, M.default_props(c, { position = "bottom" }))
+    local overrides =  {
+      position = "bottom",
+      size = SIZE / 2,
+    }
+    awful.titlebar(c, M.default_props(c, overrides))
   else
     awful.titlebar(c, M.default_props(c))
   end
